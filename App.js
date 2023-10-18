af3bb3f9a45f13226af19d97551fa8378a22f6a0
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2023-10-17 21:30:46 -0400

    Update dependencies

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Closet Space</Text>
       <StatusBar style="auto" />
     </View>
   );
