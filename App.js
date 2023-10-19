81cce951bb061dadd085f0b22442bc3b80d61fd8
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2023-10-19 15:37:48 -0400

    Created landing page

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Closet Space</Text>
+      
+      <View
+      style={{
+        backgroundColor: "#F4F4F4",
+        height: 150,
+        width: 250,
+        borderRadius: 10,
+
+    
+    }}
+      >
+        <TextInput
+        style={{
+          backgroundColor: "#E3E3E3",
+          height: 40,
+          width: "75%",
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginTop: 5, 
+          marginBottom: 5,  
+        }}
+        defaultValue="Username or Email"
+      />
+      <TextInput
+        style={{
+          backgroundColor: "#E3E3E3",
+          height: 40,
+          width: "75%",
+          borderColor: 'gray',
+          borderWidth: 1, 
+          marginTop: 5, 
+          marginBottom: 5,  
+        }}
+        defaultValue="Password"
+      />
+      <Button
+      title = "Login"/>
+      </View>
+
+
+      <View>
+        <Button
+        title="Create an account"
+        color={"#aaffd6"}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
