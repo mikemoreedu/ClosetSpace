315fb80f47118246cc7f6864a42d99b3cf6301d8
juan0campo8 <jocampo3@stevens.edu>
2023-10-31 18:48:52 -0400

    pipeline deliver changed

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,12 +2,22 @@
 
 echo 'The following command will initiate a build of the app on a ios sim'
 
+set -x 
+eas login
+set +x
+
+echo jocampo8
+echo jO#203500
+
+
 set -x
-eas build -p ios --profile preview
+#eas build -p ios --profile preview
+eas build -p android --profile preview
 set +x
 
 echo 'This will run the build latest build'
 
 set -x
-eas build:run -p ios --latest
+#eas build:run -p ios --latest
+eas build:run -p android --latest
 set +x
