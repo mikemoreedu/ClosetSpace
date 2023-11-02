3b9b034ddf36f85582e2a75c416742629c21dd54
juan0campo8 <jocampo3@stevens.edu>
2023-11-02 15:19:28 -0400

    add expo start script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,8 +6,9 @@ echo 'The following command will initiate a build of the app on a ios sim'
 set -x
 #eas build -p ios --profile preview
 npx expo login -u jocampo8 -p jO#203500
-eas init
-eas build -p android --profile preview --non-interactive
+npx expo start
+#eas build:configure
+#eas build:configure -p android --profile preview --non-interactive
 set +x
 
 echo 'This will run the build latest build'
